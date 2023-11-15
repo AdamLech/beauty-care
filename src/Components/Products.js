f46ee25030a49bea31f532ebb16a74dc465c9d73
adam lech <adammichallech@gmail.com>
2023-11-15 11:30:47 +0100

    Added home page stuff and made html struct for prd

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,28 @@
 import React from "react";
+import "../css/Products.css";
+import image1 from "../img/image1";
 
 export const Products = (props) => {
-  return <div className="-view">Skurwiel </div>;
+  return (
+    <div className="products-page-main-container">
+      <div className="products-functions">
+        <div className="products-search"></div>
+        <div className="products-filters"></div>
+      </div>
+      <div className="products-body">
+        <div className="products-categories">
+          <div className="products-categories-cat1">Category 1</div>
+          <div className="products-categories-cat1">Category 2</div>
+          <div className="products-categories-cat1">Category 3</div>
+          <div className="products-categories-cat1">Category 4</div>
+          <div className="products-categories-cat1">Category 5</div>
+        </div>
+        <div className="products-body-panel">
+          <div className="panel-product">
+            <img src={image1} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
